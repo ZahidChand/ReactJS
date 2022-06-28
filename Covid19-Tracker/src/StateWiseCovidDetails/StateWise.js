c9b081222a8070320ab13c5a81f65fae295e5709
Zahid Chand <87262433+ZahidChand@users.noreply.github.com>
2022-06-28 10:23:18 +0530

    Add files via upload

---
+++ Covid19-Tracker/src/StateWiseCovidDetails/StateWise.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import "./StateWise.css"
+export const StateWise = () => {
+
+    const [data, setData] = useState([]);
+
+    const getCovidData = async () => {
+
+        const res = await fetch('https://data.covid19india.org/data.json');
+        const covidData = await res.json();
+        console.log(covidData);
+        setData(covidData.statewise)
+    }
+    useEffect(() => {
+
+        getCovidData();
+
+    }, []);
+
+
+    return (
+        <div>
+
+            <div className='container-fluid mt-5' >
+                <div className='main-heading'>
+                    <h1 className='mb-5 text-center'><span className='font-weidth-bold'> INDIA</span> Covid-19 Dashboard</h1>
+                </div>
+                <div className='table-responsive'>
+                    <table className='table table-hover'>
+                        <thead className='thead-dark'>
+                            <tr>
+                                <th>State</th>
+                                <th>Confirmed</th>
+                                <th>Recovered</th>
+                                <th>Death</th>
+                                <th>Active</th>
+                                <th>Updated</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {
+                                data.map((currentVal, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <th>{currentVal.state}</th>
+                                            <td>{currentVal.confirmed}</td>
+                                            <td>{currentVal.recovered}</td>
+                                            <td>{currentVal.deaths}</td>
+                                            <td>{currentVal.active}</td>
+                                            <td>{currentVal.lastupdatedtime}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div >
+    )
+}
+export default StateWise;
